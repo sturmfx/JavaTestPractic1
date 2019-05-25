75e05dd7142a06a07da179497d3698e5e1afae33
sturmfx <ttunikitashtobert@gmail.com>
2019-05-25 12:24:34 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,56 @@
+public class Main {
+
+    public int[] numbers = {67, 34, 23, 12, 98, 27, 9, 3, 6, 24};
+
+    public String test_string =  "This is just test String to text students";
+
+    public static void main(String[] args)
+    {
+
+        //Протестируйте все функции используя System.out.println(); для вывода результатов выполнения функций в консоль
+
+    }
+
+    public int sum(int[] num)
+    {
+        int result = 0;
+        //Эта функция должна вернуть сумму всех чисел массива который был подан как аргумент функции
+        return result;
+    }
+
+    public int smallest(int[] num)
+    {
+        int result = 0;
+        //Эта функция должна вернуть наименьшее из всех чисел массива который был подан как аргумент функции
+        return result;
+    }
+
+    public int largest(int[] num)
+    {
+        int result = 0;
+        //Эта функция должна вернуть наибольшее из всех чисел массива который был подан как аргумент функции
+        return result;
+    }
+
+    public int average(int[] num)
+    {
+        int result = 0;
+        //Эта функция должна вернуть среднее арифметическое из всех чисел массива который был подан как аргумент функции
+        return result;
+    }
+
+    static class Person
+    {
+        //Дополните класс так чтобы при создании экзэмпляра можно было вызвать функцию print(); котороая вывела бы такой результат
+        //ID:***, FULLNAME = {**********}
+        public Person(int id, String name, String second_name)
+        {
+
+        }
+        public void print()
+        {
+            System.out.println();
+        }
+    }
+
+}
